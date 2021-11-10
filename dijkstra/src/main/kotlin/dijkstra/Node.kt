d500d26c730dc9a32dc4546dc28b2440ce1355be
balintv <balint.valoczy@connectis.nl>
2021-11-10 13:34:37 +0100

    Dijkstra

---
+++ dijkstra/src/main/kotlin/dijkstra/Node.kt
@@ -0,0 +1,32 @@
+package dijkstra
+
+class Node(
+    var value: String,
+    val neighbours: MutableMap<Node, Int> = mutableMapOf(),
+    var visited: Boolean = false
+) {
+    fun findCheapestPaths(cheapestPaths: MutableMap<Node, Int>) {
+        this.visited = true
+
+        val sortedNeighbours = mutableMapOf<Node, Int>()
+
+        neighbours.entries.sortedBy { it.value }.forEach {
+            sortedNeighbours[it.key] = it.value
+        }
+
+        sortedNeighbours.forEach {
+            if (!it.key.visited) {
+                if (cheapestPaths[it.key] == 0) {
+                    cheapestPaths[it.key] = cheapestPaths[this]!! + it.value
+                } else if (cheapestPaths[this]!! + it.value < cheapestPaths[it.key]!!) {
+                    cheapestPaths[it.key] = cheapestPaths[this]!! + it.value
+                }
+            }
+        }
+        sortedNeighbours.forEach {
+            if (!it.key.visited) {
+                it.key.findCheapestPaths(cheapestPaths)
+            }
+        }
+    }
+}
